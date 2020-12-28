2555df3182a8ea2af0a22136a477647bead60cd2
Andarb <dev@andarb.com>
2020-12-28 20:39:19 +0000

    Save bandwidth and time by storing posters in internal storage

--- app/src/main/java/com/andarb/movietinder/util/Extensions.kt
+++ app/src/main/java/com/andarb/movietinder/util/Extensions.kt
@@ -1,12 +1,47 @@
 package com.andarb.movietinder.util
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import java.io.File
+import java.io.FileOutputStream
+
 
 private const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/"
 private const val POSTER_SIZE = "w500" // applicable sizes "w92", "w154", "w185", "w342" and "w780"
 
-/** Simplifies downloading and displaying movie posters */
-fun ImageView.load(filePath: String?) {
-    Glide.with(this).load(POSTER_BASE_URL + POSTER_SIZE + filePath).into(this)
+/** Downloads, saves to internal storage and displays the movie poster */
+fun ImageView.download(imagePath: String?, fileId: Int) {
+    val imageView = this
+
+    Glide.with(imageView).asBitmap().load(POSTER_BASE_URL + POSTER_SIZE + imagePath)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                imageView.setImageBitmap(resource)
+
+                val outputStream: FileOutputStream
+                try {
+                    outputStream = context.openFileOutput(fileId.toString(), Context.MODE_PRIVATE)
+                    resource.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                    outputStream.close()
+                } catch (error: Exception) {
+                    error.printStackTrace()
+                }
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {}
+        })
+}
+
+/** Loads poster from internal storage if available, otherwise downloads it */
+fun ImageView.load(imagePath: String?, fileId: Int) {
+    val filePath = File(context.filesDir, fileId.toString()).absolutePath
+    val bitmap = BitmapFactory.decodeFile(filePath)
+
+    if (bitmap != null) this.setImageBitmap(bitmap) else this.download(imagePath, fileId)
 }
